9ccc2d311e296bc3a5e1ebb090a8023bf88e8e67
Sarmad Rehman <sarmadr02@gmail.com>
2022-12-31 18:21:46 +0500

    SpEL using annitation before without-XML

--- springcore/src/main/java/com/springcore/spel/demo.java
+++ springcore/src/main/java/com/springcore/spel/demo.java
@@ -8,6 +8,20 @@ public class demo {
 	@Value("#{22+11}")
 private int x;
 private int y;
+@Value("#{T(Math).sqrt(144)}")
+private int z;
+@Value("#{T(Math).E}")
+private double e;
+@Value("#{T(Math).PI}")
+private double PI;
+@Value("#{new String ('Sarmad Rehman')}")
+private String myName;
+@Value("#{5>2}")
+private boolean isActive;
+
+
+
+
 public int getX() {
 	return x;
 }
@@ -20,10 +34,41 @@ public int getY() {
 public void setY(int y) {
 	this.y = y;
 }
+
+public int getZ() {
+	return z;
+}
+public void setZ(int z) {
+	this.z = z;
+}
+
+public int getE() {
+	return e;
+}
+public void setE(int e) {
+	this.e = e;
+}
+public int getPI() {
+	return PI;
+}
+public void setPI(int pI) {
+	PI = pI;
+}
+public String getMyName() {
+	return myName;
+}
+public void setMyName(String myName) {
+	this.myName = myName;
+}
 @Override
 public String toString() {
-	return "demo [x=" + x + ", y=" + y + "]";
+	return "demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", PI=" + PI + ", myName=" + myName + ", isActive="
+			+ isActive + "]";
 }
+
+
+
+
  
 
 }
